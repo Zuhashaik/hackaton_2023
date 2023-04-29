91a3d9fcd91d74936b3c55bc17787575db99ace2
endurijahnavi <112099244+endurijahnavi@users.noreply.github.com>
2023-04-30 00:35:02 +0530

    home.py

--- home.py
+++ home.py
@@ -1,49 +1,99 @@
 import streamlit as st
+import speech_recognition as sr
+import streamlit as st
+import keras_ocr
+import matplotlib.pyplot as plt
+import numpy as np
+import io
+import pyttsx3
 
-# Define the function to redirect to Page 1
+engine = pyttsx3.init()
+
+st.title("This is a application for visually impaired individuals")
+st.header("1.Text Detection Mode")
+st.header("2.Object detection mode")
+st.header("3.Surroundings detection Mode")
+
+engine.say("This is an application for visually impaired individuals. There are three available modes: Text Detection Mode, Object detection mode, and Surroundings detection Mode. Please select your option by saying it aloud.")
+engine.runAndWait()
+
+def transcribe_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        st.write("Speak something...")
+        audio = r.listen(source)
+    try:
+        text = r.recognize_google(audio)
+        st.write(f"{text}")
+        return text
+    except:
+        st.write("Sorry, could not recognize your voice")
+        return ""
+
 def page1():
-    st.write("Text detection is like a treasure hunt in images. It's the process of finding hidden words and turning them into searchable data.")
+    st.title("Text Detection from Image")
+    # Title of the app
+    st.title("Image Uploader")
+
+    # A header for the section
+    st.header("Upload an Image")
+
+    # A file uploader for the image
+    image_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
+
+    # If an image is uploaded, show it
+    if image_file is not None:
+        # Read the image file as a bytes-like object
+        image_bytes = io.BytesIO(image_file.read())
+
+        # Use keras_ocr to read the image
+        pipeline = keras_ocr.pipeline.Pipeline()
+        images = keras_ocr.tools.read(image_bytes)
+        images = images.reshape((-1,) + images.shape)
+
+        # Recognize text in the image
+        prediction_groups = pipeline.recognize(images)
+
+        # Extract the text predictions from the recognition results
+        text_predictions = []
+        for predictions in prediction_groups:
+            prediction_text = []
+            for word in predictions:
+                prediction_text.append(word[0])
+            text_predictions.append(" ".join(prediction_text))
+
+        # Display the annotated image with the recognized text
+        st.subheader("Annotated Image with Text Predictions")
+        fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
+        axs_flat = np.ravel(axs)
+        for ax, image, predictions in zip(axs_flat, images, prediction_groups):
+            ax.imshow(image)
+            keras_ocr.tools.drawAnnotations(image=image, predictions=predictions, ax=ax)
+            ax.axis("off")
+        st.pyplot(fig)
+
+        # Display the recognized text
+        st.subheader("Text Predictions")
+        st.write("\n".join(text_predictions))
     
 # Define the function to redirect to Page 2
 def page2():
+    st.title("Person Detection")
     st.write("Spot the human: Person detection for a safer world")
     
 # Define the function to redirect to Page 3
 def page3():
+    st.title("Surroundings Detection")
     st.write("Discover hidden secrets in your surroundings with cutting-edge surroundings detection technology")
+if __name__ == '__main__':
+    user_input = transcribe_audio()
 
-# Set up the layout of the UI with 3 containers
-st.set_page_config(page_title="Hackathon", page_icon=":smiley:", layout="wide")
-col1, col2, col3 = st.columns(3)
+    # Display buttons based on user input
+    if user_input:
+        if "text detection mode" in user_input:
+            page1()
+        if "object detection mode" in user_input:
+            page2()
+        if "surroundings detection mode" in user_input:
+            page3()
 
-# Add a button to the first container that redirects to Page 1
-with col1:
-    st.write("Text Detection in Image")
-    if st.button("Try TDI"):
-        st.session_state.page = "page1"
-
-# Add a button to the second container that redirects to Page 2
-with col2:
-    st.write("Person Detection")
-    if st.button("Try PD"):
-        st.session_state.page = "page2"
-
-# Add a button to the third container that redirects to Page 3
-with col3:
-    st.write("Surroundings Detection")
-    if st.button("Try SD"):
-        st.session_state.page = "page3"
-    
-# Check which page to show based on the button clicked
-if "page" in st.session_state:
-    if st.session_state.page == "page1":
-        page1()
-        
-    elif st.session_state.page == "page2":
-        page2()
-        
-    elif st.session_state.page == "page3":
-        page3()
-        
-else:
-    st.write("Welcome to the Main Page!")
