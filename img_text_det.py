e4e2e88756f9583915f1c1e20c84ead91dabaf93
endurijahnavi <112099244+endurijahnavi@users.noreply.github.com>
2023-05-03 22:43:02 +0530

    Update img_text_det.py

--- img_text_det.py
+++ img_text_det.py
@@ -4,37 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import io
 
-# Title of the app
 st.title("Image Uploader")
 
-# A header for the section
 st.header("Upload an Image")
 
-# A file uploader for the image
 image_file = st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])
 
-# If an image is uploaded, show it
 if image_file is not None:
-    # Read the image file as a bytes-like object
     image_bytes = io.BytesIO(image_file.read())
 
-    # Use keras_ocr to read the image
     pipeline = keras_ocr.pipeline.Pipeline()
     images = keras_ocr.tools.read(image_bytes)
     images = images.reshape((-1,) + images.shape)
 
-    # Recognize text in the image
     prediction_groups = pipeline.recognize(images)
 
-    # Extract the text predictions from the recognition results
     text_predictions = []
     for predictions in prediction_groups:
         prediction_text = []
         for word in predictions:
             prediction_text.append(word[0])
         text_predictions.append(" ".join(prediction_text))
-
-    # Display the annotated image with the recognized text
+        
     st.subheader("Annotated Image with Text Predictions")
     fig, axs = plt.subplots(nrows=len(images), figsize=(10, 20))
     axs_flat = np.ravel(axs)
@@ -44,7 +35,6 @@ if image_file is not None:
         ax.axis("off")
     st.pyplot(fig)
 
-    # Display the recognized text
     st.subheader("Text Predictions")
     st.write("\n".join(text_predictions))
 
